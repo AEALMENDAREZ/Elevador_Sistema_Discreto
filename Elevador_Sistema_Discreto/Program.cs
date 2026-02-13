51ac38e87ff0fa45cd41f4b7260f42daedd29d69
Ariel Almendárez <arielalmendarez0502@gmail.com>
2026-02-13 08:24:12 -0600

    Avance en código

--- Elevador_Sistema_Discreto/Program.cs
+++ Elevador_Sistema_Discreto/Program.cs
@@ -1,4 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//HOLA
+
+using System;
+
+class Elevador 
+{
+    static int capacidadElevador = 10;
+    
+    static public bool Libre(int numeroPersonas)
+    {
+        if (numeroPersonas >= capacidadElevador)
+        {
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
+    }
+
+    static public string estadoElevador(bool libre) 
+    {
+        if (libre) 
+        {
+            return "LIBRE";
+        }
+        else
+        {
+            return "OCUPADO";
+        }
+    }
+
+    static void Main()
+    {
+        int numeroPersonas = 0;
+        Console.WriteLine("El elevador tiene una capacidad de 10 personas...");
+        Console.WriteLine("");
+        Console.WriteLine("Entran 8 personas al elevador");
+        Console.WriteLine("El elevador se encuentra en estado: " + estadoElevador(Libre(numeroPersonas)));
+    }
+
+}
